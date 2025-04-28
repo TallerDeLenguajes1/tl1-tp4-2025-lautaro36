edbe298d295b4244604607ae150668039b6cb0bc
lautaro anazgo <lautaroanazgo@gmail.com>
2025-04-28 00:15:40 -0300

    ahora si funcionan las fn de buscar cargar, buscar e insertar

--- tp2_1.c
+++ tp2_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct {
     int TareaID;//Numérico autoincremental comenzando en 1000
@@ -13,43 +14,23 @@ struct Nodo {
     }typedef Nodo;
 
 Tarea cargarTarea();
-Nodo *crearNodo(Tarea tarea);
-void insertarNodo(Nodo **start, Nodo *tarea);
-Nodo *buscarNodo(Nodo **start, int id);
-void quitarNodo(Nodo **start, Nodo *nodo);
+Nodo *crearNodo(Tarea nuevaTarea);
+void insertarNodo(Nodo **lista, Nodo *nuevoNodo);
+Nodo *buscarNodo(Nodo **lista, int id);
+Nodo *quitarNodo(Nodo *nodo);
+void mostrarNodos(Nodo *lista);
 
 int tareasId = 1000;
 
 int main() {
     char bandera='s';
-    Tarea nuevaTarea;
-    Nodo *start = NULL;
-
-    printf("Ingrese una nueva tarea\n");
-    
+    Nodo *lista = NULL;
     while(bandera == 'S' || bandera == 's') {
-        nuevaTarea = cargarTarea();
-        insertarNodo(&start, crearNodo(nuevaTarea));
+        insertarNodo(&lista, crearNodo(cargarTarea() ));
         printf("Desea ingresar otra tarea? S/N \n");
         scanf(" %c", &bandera);
     }
-
-    Nodo *startRealizadas = NULL;
-    int idRealizada;
-    puts("pasar alguna tarea pendiente a la lista de realizadas ?S/N");
-    scanf(" %c", &bandera);
-    while(bandera == 'S' || bandera == 's') {
-        puts("Ingrese el ID de la tarea realizada.");
-        scanf("%d", &idRealizada);
-        insertarNodo(&startRealizadas, buscarNodo(&start, idRealizada));
-    }
-
-    /*Nodo *aux = start;
-    while(aux->Siguiente) {
-        aux = aux->Siguiente;
-        printf("\nTarea %d: %s\tDuracion:%d", aux->T.TareaID, aux->T.Descripcion, aux->T.Duracion);
-    } muestro por pantalla
-    */
+    mostrarNodos(lista);
 
     /*if(buscarNodo(&start, idRealizada)) 
     printf("\nTarea %d: %s\tDuracion:%d",buscarNodo(&start, idRealizada)->T.TareaID, buscarNodo(&start, idRealizada)->T.Descripcion, buscarNodo(&start, idRealizada)->T.Duracion);
@@ -59,15 +40,16 @@ int main() {
 }
 
 Tarea cargarTarea() {
+    char *buff;
     Tarea nuevaTarea;
-    nuevaTarea.Descripcion = (char *) malloc (sizeof(char)*100);
+    buff = (char *) malloc (sizeof(char)*100);
     printf("Ingrese una descripcion de la tarea:");
-    fflush(stdin);
-    gets(nuevaTarea.Descripcion);
+    scanf("%s", buff);
+    nuevaTarea.Descripcion = (char *) malloc (sizeof(char)*(strlen(buff)) +1);
+    strcpy(nuevaTarea.Descripcion, buff);
     
     printf("Ingrese una duracion de la tarea:");
     scanf("%d", &nuevaTarea.Duracion);
-    fflush(stdin);
     
     nuevaTarea.TareaID = tareasId;
     tareasId++;
@@ -77,29 +59,40 @@ Tarea cargarTarea() {
 
 Nodo *crearNodo(Tarea nuevaTarea) {
     Nodo *nuevoNodo = (Nodo *) malloc(sizeof(Nodo));
-    nuevoNodo->T = nuevaTarea;
+    nuevoNodo->T.Descripcion = (char *) malloc (sizeof(char)*(strlen(nuevaTarea.Descripcion)) +1);
+    strcpy(nuevoNodo->T.Descripcion, nuevaTarea.Descripcion);
+    nuevoNodo->T.Duracion = nuevaTarea.Duracion;
+    nuevoNodo->T.TareaID = nuevaTarea.TareaID;
     nuevoNodo->Siguiente = NULL;
     return nuevoNodo;
 }
 
-void insertarNodo(Nodo **start, Nodo *nuevaTarea) {
-    nuevaTarea->Siguiente = *start;
-    *start = nuevaTarea;
+void insertarNodo(Nodo **lista, Nodo *nuevoNodo) {
+    nuevoNodo->Siguiente = *lista;
+    *lista = nuevoNodo;
 }
 
-Nodo *buscarNodo(Nodo **start, int id) {
-    Nodo **aux = start;
+Nodo *buscarNodo(Nodo **lista, int id) {
+    Nodo **aux = lista;
     while(aux && (*aux)->T.TareaID != id){
         *aux = (*aux)->Siguiente;
     }
     return *aux;
 }
 
-void quitarNodo(Nodo **start, Nodo *nodo) {
-    Nodo **aux = start;
-
-    if(*aux) {
-
+Nodo *quitarNodo(Nodo *nodo) {
+    if(nodo) {
+        Nodo *temp = nodo;
+        nodo = nodo->Siguiente;
+        temp->Siguiente = NULL;
+        return temp;
     }
+    return NULL;
+}
 
+void mostrarNodos(Nodo *lista) {
+    while(lista) {
+        printf("\nTarea %d: %s\tDuracion:%d", lista->T.TareaID, lista->T.Descripcion, lista->T.Duracion);
+        lista = lista->Siguiente;
+    }
 }
